dbdd98be66766525a439984005ce8d075bf722fb
CatCylla <matthrewgreenwood@gmail.com>
2022-09-16 07:59:15 +0800

    真删除/插入

--- api_demo/routes/index.js
+++ api_demo/routes/index.js
@@ -22,4 +22,36 @@ router.get("/api/getStuList", (req, res) => {
   });
 });
 
+/**
+ * 往数据库中插入学生记录
+ */
+router.post("/api/insertStuList", function (req, res) {
+  conn.query("INSERT INTO students SET  ?", req.body, (err, results) => {
+    if (err)
+      return res.json({
+        err_code: 1,
+        message: "插入数据失败",
+        affextedRows: 0,
+      });
+    res.json({ success_code: 200, message: "插入成功" });
+  });
+});
+
+/**
+ * 删除数据库中的一条学生记录
+ */
+router.post("/api/delStuList", (req, res) => {
+  console.log(req.body);
+  let sqlStr = "DELETE FROM students WHERE id = ?";
+  conn.query(sqlStr, [req.body.id], (err, results) => {
+    if (err)
+      return res.json({
+        err_code: 1,
+        message: "删除数据失败",
+        affextedRows: 0,
+      });
+    res.json({ success_code: 200, message: "删除数据失败", affextedRows: 0 });
+  });
+});
+
 module.exports = router;
